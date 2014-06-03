dac5eeb52cfc4ec8823e0ab85129f3b66aaf540c
morganric <morganric@gmail.com>
2014-06-03 20:42:51 +0100

    conflicted

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,3 +1,4 @@
+ENV['PANDASTREAM_URL'] = "https://e316004b0285a30b60e3:3165cec2e770620d6973@api.pandastream.com/59058807b78fc5405c6b63cf44140873"
 ENV['ADMIN_NAME'] = 'First User'
 ENV['ADMIN_EMAIL']='user@example.com' 
 ENV['ADMIN_PASSWORD']='changeme'
