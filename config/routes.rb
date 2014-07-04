dbb92afd169ed66aabd287ecf894877ddfae4e41
morganric <morganric@gmail.com>
2014-07-04 15:37:12 +0100

    fix

--- config/routes.rb
+++ config/routes.rb
@@ -9,7 +9,7 @@ Rails.application.routes.draw do
   # resources :facebook_page
 
   post '/facebook_page' => 'facebook_page#create', :as => 'facebook_page'
-  get 'facebook' => 'profiles#facebook', :as => 'facebook'
+  post 'facebook' => 'profiles#facebook', :as => 'facebook'
   get 'apps' => 'profiles#apps', :as => 'apps'
   get 'admin' => 'leafs#admin', :as => 'admin'
 
