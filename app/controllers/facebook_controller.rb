52696194308d336525a411893a5a444d1e9b8b58
morganric <morganric@gmail.com>
2014-07-07 12:54:36 +0100

    fix

--- app/controllers/facebook_controller.rb
+++ app/controllers/facebook_controller.rb
@@ -38,7 +38,7 @@ class FacebookController < ApplicationController
 
         @user = User.find(@user_id)
 
-        redirect_to facebook_url_path(@user)
+        redirect_to facebook_url_path(@user.id)
       end
 
     else
@@ -55,23 +55,23 @@ class FacebookController < ApplicationController
   # GET /profiles/1.json
   def show
      @params = params
-    #  signed_request = params[:signed_request] 
-    #  @signed_request = decode_data(signed_request)
+     signed_request = params[:signed_request] 
+     @signed_request = decode_data(signed_request)
     
-    # if params[:id] != "tagged" #weak
-    #   @profile = Profile.find(params[:id])
-    #   @posts = @profile.user.posts.order("created_at DESC").page(params[:page]).per(6)
-    # end
+    if params[:id] != "tagged" #weak
+      @profile = Profile.find(params[:id])
+      @posts = @profile.user.posts.order("created_at DESC").page(params[:page]).per(6)
+    end
     
-    # respond_to do |format|
-    #   if @profile
-    #       format.html # show.html.erb
-    #       format.json { render json: vanity_url_path(@profile) }
-    #   else
-    #       format.html { redirect_to root_url }
-    #       format.json { render json: vanity_url_path(@profile).errors, status: :unprocessable_entity }
-    #   end
-    # end
+    respond_to do |format|
+      if @profile
+          format.html # show.html.erb
+          format.json { render json: vanity_url_path(@profile) }
+      else
+          format.html { redirect_to root_url }
+          format.json { render json: vanity_url_path(@profile).errors, status: :unprocessable_entity }
+      end
+    end
   end
 
 
