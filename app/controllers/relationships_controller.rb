88fdfaa8a0216005b37feceed8cca8982121360c
morganric <morganric@gmail.com>
2014-07-22 14:09:41 +0100

    fix

--- app/controllers/relationships_controller.rb
+++ app/controllers/relationships_controller.rb
@@ -8,13 +8,13 @@ class RelationshipsController < ApplicationController
   def create
     @user = User.find(params[:relationship][:followed_id])
     current_user.follow!(@user)
-    redirect_to vanity_url_path(@user)
+    redirect_to vanity_url_path(@user.profile.slug)
   end
 
   def destroy
     @user = Relationship.find(params[:id]).followed
     current_user.unfollow!(@user)
-    redirect_to vanity_url_path(@user)
+    redirect_to vanity_url_path(@user.profile.slug)
   end
 
   def follow_tweet
