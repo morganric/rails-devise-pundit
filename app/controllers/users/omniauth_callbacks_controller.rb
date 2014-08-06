1c25e0ed3d5a73176ddf25495e70340ff00c8ed1
morganric <morganric@gmail.com>
2014-08-06 13:39:00 +0100

    fb

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -4,17 +4,31 @@ class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
     # You need to implement the method below in your model (e.g. app/models/user.rb)
     # @user = User.find_for_facebook_oauth(request.env["omniauth.auth"])
 
-    @user = current_user
-    @user.fb_token = request.env["omniauth.auth"].credentials.token
-    @user.save
+    if current_user
+	    @user = current_user
+	    @user.fb_token = request.env["omniauth.auth"].credentials.token
+	    @user.save
 
-    if @user.persisted?
-      sign_in_and_redirect @user, :event => :authentication #this will throw if @user is not activated
-      set_flash_message(:notice, :success, :kind => "Facebook") if is_navigational_format?
-    else
-      session["devise.facebook_data"] = request.env["omniauth.auth"]
-      redirect_to new_user_registration_url
-    end
+	    if @user.persisted?
+	      sign_in_and_redirect @user, :event => :authentication #this will throw if @user is not activated
+	      set_flash_message(:notice, :success, :kind => "Facebook") if is_navigational_format?
+	    else
+	      session["devise.facebook_data"] = request.env["omniauth.auth"]
+	      redirect_to new_user_registration_url
+	    end
+	else
+		auth = request.env["omniauth.auth"]
+		@user = User.find_for_facebook_oauth(request.env["omniauth.auth"])
+
+		if @user.persisted?
+		  sign_in_and_redirect @user, :event => :authentication #this will throw if @user is not activated
+	      set_flash_message(:notice, :success, :kind => "Facebook") if is_navigational_format?
+	    else
+	      session["devise.facebook_data"] = request.env["omniauth.auth"]
+	      redirect_to new_user_registration_url
+	    end
+
+	end
   end
 
 	def twitter
